53ebfd522addde7df1a1662c2a9ea1cc9075d69b
Roman Lazuka <rn.lazuka@gmail.com>
2019-12-13 16:26:57 +0300

    first commit

---
+++ src/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import s from './Header.module.css'
+
+function Header () {
+  return (
+    <div className={s.header}>
+
+    </div>
+  );
+}
+
+export default Header;
