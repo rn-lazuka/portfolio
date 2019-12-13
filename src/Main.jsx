98de35fe56d2dcce4e6d04535a147ca97bc00b98
Roman Lazuka <rn.lazuka@gmail.com>
2019-12-13 20:43:28 +0300

    added flex-box and some style for header and main

--- src/Main.jsx
+++ src/Main.jsx
@@ -4,7 +4,16 @@ import s from './Main.module.css'
 function Main() {
     return (
         <div className={s.main}>
-
+            <div className={s.container}>
+                <div className={s.greeting}>
+                    <span>Привет</span>
+                    <span>Меня зовут Роман Лазука</span>
+                    <span>Я front-end разработчик</span>
+                </div>
+                <div className={s.photo}>
+                    <img src="" alt=""/>
+                </div>
+            </div>
         </div>
     );
 }
