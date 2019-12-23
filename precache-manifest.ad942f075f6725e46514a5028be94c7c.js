a17e992f3e72bdf8bab1206e9535613e7e95aaf5
Roman Lazuka <rn.lazuka@gmail.com>
2019-12-23 14:19:23 +0300

    Updates

---
+++ precache-manifest.ad942f075f6725e46514a5028be94c7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c160eec93534d9a3373856490c6c5384",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2d699cbab8ba7767efde",
+    "url": "/portfolio/static/css/main.69bc2da5.chunk.css"
+  },
+  {
+    "revision": "a851947ec8f5975270b5",
+    "url": "/portfolio/static/js/2.f6988237.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.f6988237.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d699cbab8ba7767efde",
+    "url": "/portfolio/static/js/main.fae9d91d.chunk.js"
+  },
+  {
+    "revision": "16c109c584451046bd84",
+    "url": "/portfolio/static/js/runtime-main.860ade9b.js"
+  }
+]);
