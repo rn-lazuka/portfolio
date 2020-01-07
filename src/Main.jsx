9f4c884a53bc664631374a675897efbd4928bcd6
Roman Lazuka <rn.lazuka@gmail.com>
2020-01-07 16:08:54 +0300

    added fonts, changed header and main style

--- src/Main.jsx
+++ src/Main.jsx
@@ -6,9 +6,9 @@ function Main() {
         <div className={s.main}>
             <div className={s.container}>
                 <div className={s.greeting}>
-                    <span>Привет</span>
-                    <span>Меня зовут Роман Лазука</span>
-                    <span>Я front-end разработчик</span>
+                    <span>Привет!</span>
+                    <span>Меня зовут <span>Роман Лазука</span></span>
+                    <h1>Я front-end разработчик</h1>
                 </div>
                 <div className={s.photo}>
                     <img src="" alt=""/>
