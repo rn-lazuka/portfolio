f767f48012f3859e472a172e70998c170f678e59
Roman Lazuka <rn.lazuka@gmail.com>
2020-02-12 17:54:06 +0300

    added Particles for App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Particles from 'react-particles-js';
 import './App.css';
 import Header from "./Header";
 import Main from "./Main";
@@ -9,10 +9,105 @@ import Slogan from "./Slogan";
 import Contacts from "./Contacs";
 import Footer from "./Footer";
 
+
+const particleOpt = {
+    "particles": {
+        "number": {
+            "value": 80,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#ffffff"
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            },
+            "polygon": {
+                "nb_sides": 3
+            },
+            "image": {
+                "src": "img/github.svg",
+                "width": 100,
+                "height": 100
+            }
+        },
+
+
+        "line_linked": {
+            "enable": true,
+            "distance": 175,
+            "color": "#ffffff",
+            "opacity": 0.15,
+            "width": 2
+        },
+        "move": {
+            "enable": true,
+            "speed": 4,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "repulse"
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "push"
+            },
+            "resize": true
+        },
+        "modes": {
+            "grab": {
+                "distance": 100,
+                "line_linked": {
+                    "opacity": 1
+                }
+            },
+            "bubble": {
+                "distance": 800,
+                "size": 80,
+                "duration": 2,
+                "opacity": 0.8,
+                "speed": 3
+            },
+            "repulse": {
+                "distance": 100,
+                "duration": 0.4
+            },
+            "push": {
+                "particles_nb": 4
+            },
+            "remove": {
+                "particles_nb": 2
+            }
+        }
+    },
+    "retina_detect": true
+};
+
 function App() {
     return (
         <div className="App">
             <div className="portfolio">
+                <Particles className={"particles"} params={particleOpt}/>
                 <Header/>
                 <Main/>
                 <Skills/>
