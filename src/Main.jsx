836843a7d8d4df92c9c6ae1d886c6c6a3de0a6f7
Roman Lazuka <rn.lazuka@gmail.com>
2020-02-15 17:51:16 +0300

    added Tilt for Main photo

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import s from './Main.module.css'
+import Tilt from 'react-tilt'
+import profile from './assets/img/benderProfile.jpg'
 
 function Main() {
     return (
-        <div className={s.main}>
+        <div id={"main"} className={s.main}>
             <div className={s.container}>
                 <div className={s.greeting}>
                     <span>Привет!</span>
@@ -11,9 +13,23 @@ function Main() {
                     <h1>Я front-end разработчик</h1>
                 </div>
                 <div className={s.photo}>
-                    <div className={s.image}>
-
-                    </div>
+                    <Tilt className="Tilt" options={{
+                        reverse: false,  // reverse the tilt direction
+                        max: 35,     // max tilt rotation (degrees)
+                        perspective: 1000,   // Transform perspective, the lower the more extreme the tilt gets.
+                        scale: 1,      // 2 = 200%, 1.5 = 150%, etc..
+                        speed: 300,    // Speed of the enter/exit transition
+                        transition: true,   // Set a transition on enter/exit.
+                        axis: null,   // What axis should be disabled. Can be X or Y.
+                        reset: true,   // If the tilt effect has to be reset on exit.
+                        easing: "cubic-bezier(.03,.98,.52,.99)"
+                    }} style={{height: 250, width: 250}}>
+                        <div className="Tilt-inner">
+                            <div className={s.image} style={{
+                                backgroundImage: `url(${profile})`
+                            }}/>
+                        </div>
+                    </Tilt>
                 </div>
             </div>
         </div>
--- src/NavMenu.jsx
+++ src/NavMenu.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import s from './NavMenu.module.css'
 
+
 function NavMenu() {
     return (
         <div className={s.navMenu}>
-            <a className={s.link} href="">Главная</a>
-            <a className={s.link} href="">Скилы</a>
-            <a className={s.link} href="">Проекты</a>
-            <a className={s.link} href="">Контакты</a>
+            <a className={s.link} href="#">Главная</a>
+            <a className={s.link} href="#">Скилы</a>
+            <a className={s.link} href="#">Проекты</a>
+            <a className={s.link} href="#">Контакты</a>
         </div>
     );
 }
